85c83ffe0aa49259bfb8cd1728000976d9a48b7f
Angel <116260834+angeelrdgzz@users.noreply.github.com>
2025-02-12 21:53:35 +0100

    Update weatherForecast.test.js

--- tests/weatherForecast.test.js
+++ tests/weatherForecast.test.js
@@ -1,5 +1,4 @@
 const fetchMock = require("jest-fetch-mock");
-
 fetchMock.enableMocks();
 
 const { obtenInformacionMeteo, procesaDireccionViento } = require("../src/tiempo");
@@ -18,26 +17,18 @@ const mockRespuestaAPI = {
   json: jest.fn().mockResolvedValue(mockDatosAPI),
 };
 
-// Tests
 describe("obtenInformacionMeteo", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-    fetch.resetMocks();
-  });
-
   it("debería procesar los datos del clima correctamente", async () => {
     fetch.mockResolvedValue(mockRespuestaAPI);
-
     console.log = jest.fn();
+
     await obtenInformacionMeteo(42.2576, -8.683);
 
-    
     expect(fetch).toHaveBeenCalledWith(
       expect.stringContaining(
         "https://api.open-meteo.com/v1/forecast?latitude=42.2576&longitude=-8.683&current_weather=true"
       )
     );
-
     expect(console.log).toHaveBeenCalledWith("Temperatura:", "20 °C");
     expect(console.log).toHaveBeenCalledWith("Velocidad del viento:", "15 km/h");
     expect(console.log).toHaveBeenCalledWith("Dirección del viento: Noreste (45°)");
@@ -45,8 +36,8 @@ describe("obtenInformacionMeteo", () => {
 
   it("debería manejar errores de la API", async () => {
     fetch.mockRejectedValue(new Error("Fallo en la red"));
-
     console.error = jest.fn();
+
     await obtenInformacionMeteo(42.2576, -8.683);
 
     expect(console.error).toHaveBeenCalledWith(
@@ -59,17 +50,14 @@ describe("obtenInformacionMeteo", () => {
       ok: true,
       json: jest.fn().mockResolvedValue({}),
     });
-
     console.error = jest.fn();
+
     await obtenInformacionMeteo(42.2576, -8.683);
-/*Test expect tohaveBeenCalledWith*/
-    expect(console.error).toHaveBeenCalledWith(
-      "Datos meteorológicos no encontrados."
-    );
+
+    expect(console.error).toHaveBeenCalledWith("Datos meteorológicos no encontrados.");
   });
- 
 });
-/*Test expect toBe*/
+
 describe("procesaDireccionViento", () => {
   it("debería devolver la dirección correcta para grados dados", () => {
     expect(procesaDireccionViento(0)).toBe("Norte");
@@ -86,13 +74,13 @@ describe("procesaDireccionViento", () => {
     expect(procesaDireccionViento(-10)).toBe("Desconocido");
     expect(procesaDireccionViento(400)).toBe("Desconocido");
   });
-  /*Test expect toThrow*/
-   it("debería lanzar un error si los parámetros no son válidos", async () => {
+
+  it("debería lanzar un error si los parámetros no son válidos", async () => {
     await expect(obtenInformacionMeteo("string", {})).rejects.toThrow(
       "Latitud y longitud deben ser números válidos"
     );
   });
-/*Test expect not*/
+
   it("no debería devolver valores incorrectos", () => {
     expect(procesaDireccionViento(0)).not.toBe("Este");
     expect(procesaDireccionViento(90)).not.toBe("Norte");
