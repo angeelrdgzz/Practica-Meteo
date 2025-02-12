7875e0157c95a18a711029319b2d6f9d9bd64c63
Angel <116260834+angeelrdgzz@users.noreply.github.com>
2025-02-12 17:37:01 +0100

    Update weatherForecast.test.js

--- tests/weatherForecast.test.js
+++ tests/weatherForecast.test.js
@@ -18,18 +18,6 @@ const mockRespuestaAPI = {
   json: jest.fn().mockResolvedValue(mockDatosAPI),
 };
 
-const mockCodigosTiempo = {
-  0: "Cielo despejado",
-  1: "Mayormente despejado",
-  2: "Parcialmente nublado",
-  3: "Nublado",
-  45: "Niebla",
-  48: "Niebla con escarcha",
-  51: "Lluvia ligera",
-  53: "Lluvia moderada",
-  55: "Lluvia intensa",
-};
-
 // Tests
 describe("obtenInformacionMeteo", () => {
   beforeEach(() => {
